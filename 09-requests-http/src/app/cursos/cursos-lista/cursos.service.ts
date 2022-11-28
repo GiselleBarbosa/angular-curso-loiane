3d6702916f503176e121288150a84cb834036e92
GiselleBarbosa <g88barbosa@gmail.com>
2022-11-28 07:51:48 -0300

    feat: config environment

--- 09-requests-http/src/app/cursos/cursos-lista/cursos.service.ts
+++ 09-requests-http/src/app/cursos/cursos-lista/cursos.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from './curso';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API  = "http://localhost:3000/cursos";
+  private readonly API  = `${environment.API}/cursos`
 
   constructor(private http: HttpClient) { }
 
