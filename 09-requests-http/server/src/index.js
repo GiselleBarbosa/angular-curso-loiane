1382958a179f52c9ccc05d4920a2ab916d914f32
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-06 15:38:16 -0300

    feat: server-upload works

---
+++ 09-requests-http/server/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multiparty = require('connect-multiparty');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: '*', 
+  optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+const multipartyMiddleware = multiparty({ uploadDir: './uploads'})
+
+app.post('/upload', multipartyMiddleware, (req, res) => {
+  const files  = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.use((err, req, res, next) => res.json({
+  error: err.message
+}))
+
+app.listen(8000, () => {
+  console.log('Servidor porta 8000 esta sendo executado');
+})
--- 09-requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ 09-requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from './upload-file.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.css']
 })
-export class UploadFileComponent implements OnInit {
+export class UploadFileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+files!: Set<File> 
+inscricao!: Subscription;
 
+  constructor( private service: UploadFileService ) { }
+ 
   ngOnInit() {
   }
 
+  onChange(event: any) {
+    console.log(event);
+
+    const selectedFiles = <FileList>event.srcElement.files;
+    // document.getElementById('customFileLabel')!.innerHTML  = selectedFiles[0].name;
+
+    const fileNames = [];
+    this.files = new Set();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
+    }
+    document.getElementById('customFileLabel')!.innerHTML = fileNames.join(', ');
+  }
+
+  onUpload(){
+    if(this.files && this.files.size > 0) {
+     this.inscricao = this.service.upload(this.files, 'http://localhost:8000/upload')
+      .subscribe(response => console.log('Upload concluído com sucesso!'));
+    }
+
+  }
+  ngOnDestroy() {
+    this.inscricao.unsubscribe()
+  }
 }
