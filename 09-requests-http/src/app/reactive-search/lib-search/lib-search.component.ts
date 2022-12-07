cd3ac5fbd0ce10a63bc6d56130becfec8fbb1ae8
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-07 17:09:27 -0300

    create formControl

--- 09-requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ 09-requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-lib-search',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibSearchComponent implements OnInit {
 
+  queryField = new FormControl();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSearch(){
+    console.log(this.queryField.value);
   }
 
 }
--- 09-requests-http/src/app/reactive-search/reactive-search.module.ts
+++ 09-requests-http/src/app/reactive-search/reactive-search.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveSearchRoutingModule } from './reactive-search-routing.module';
+import { LibSearchComponent } from './lib-search/lib-search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LibSearchComponent],
+
   imports: [
     CommonModule,
-    ReactiveSearchRoutingModule
+    ReactiveSearchRoutingModule,
+    ReactiveFormsModule
+  
   ]
 })
 export class ReactiveSearchModule { }
